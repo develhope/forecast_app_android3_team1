a5c4f357a1bf1909a4fb8dcf497a1e4fc4704594
Mauro Lanini <mallettomachico@gmail.com>
2023-01-13 12:24:37 +0100

    Implementata la barra di navigazione inferiore

--- app/src/main/java/co/develhope/meteoapp/MainActivity.kt
+++ app/src/main/java/co/develhope/meteoapp/MainActivity.kt
@@ -2,10 +2,37 @@ package co.develhope.meteoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //creo un riferimento al nav controller
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHost.navController
+
+        //creo un riferimento al widget per la bottom navigation
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigation.setupWithNavController(navController)
+
+
+
+    }
+
+
+    /**
+     * Questo metodo controlla le destinazioni della bottom navigation
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/co/develhope/meteoapp/fragments/SearchFragment.kt
@@ -0,0 +1,25 @@
+package co.develhope.meteoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.develhope.meteoapp.R
+
+
+
+
+class SearchFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+
+}
