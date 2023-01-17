e09837d933698d8c591b5b4e26437ecf463efc09
Mauro Lanini <mallettomachico@gmail.com>
2023-01-17 12:13:51 +0100

    merge remote branch

--- app/src/main/java/co/develhope/meteoapp/fragments/SearchFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/fragments/SearchFragment.kt
@@ -5,20 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.R
-
-
+import co.develhope.meteoapp.adapter.RecentSearchAdapter
+import co.develhope.meteoapp.data.DataSet
+import co.develhope.meteoapp.model.RecentSearch
 
 
 class SearchFragment : Fragment() {
 
+    private lateinit var recentSearchList  :  List<RecentSearch>
+    private lateinit var recentSearchRecyclerView : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        recentSearchList = DataSet.loadRecentSearch()
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        recentSearchRecyclerView = view.findViewById(R.id.recentsearchlist)
+        val adapter = RecentSearchAdapter(recentSearchList)
+        recentSearchRecyclerView.adapter = adapter
+        recentSearchRecyclerView.layoutManager = LinearLayoutManager(view.context)
+
+
     }
 
 
