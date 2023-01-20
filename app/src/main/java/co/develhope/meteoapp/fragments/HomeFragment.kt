5adfa491053d3f31baf0b8d688be25d34d653aac
Marker97 <chereche10@gmail.com>
2023-01-20 04:44:37 +0100

    update_layout_homeScreenItem

--- app/src/main/java/co/develhope/meteoapp/fragments/HomeFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/fragments/HomeFragment.kt
@@ -6,18 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import co.develhope.meteoapp.R
+import co.develhope.meteoapp.databinding.FragmentHomeScreenBinding
 
 
 class HomeFragment : Fragment(){
-
-
+    private var bindingHomeScreen : FragmentHomeScreenBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.week_home_screen, container, false)
+        bindingHomeScreen = FragmentHomeScreenBinding.inflate(inflater,container,false )
+        return inflater.inflate(R.layout.card_home_screen, container, false)
     }
 
 
