ec8626a393047a4bc4a5cbebd07fb0bd2980685c
Mauro Lanini <mallettomachico@gmail.com>
2023-01-20 10:15:48 +0100

    Inserito titolo recycler view

--- app/src/main/java/co/develhope/meteoapp/adapter/RecentSearchAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/adapter/RecentSearchAdapter.kt
@@ -11,28 +11,27 @@ import co.develhope.meteoapp.databinding.RecentSearchItemBinding
 import co.develhope.meteoapp.model.RecentSearch
 
 
-class RecentSearchAdapter(val data : List<RecentSearch>) : RecyclerView.Adapter<RecentSearchAdapter.ViewHolder>(){
+class RecentSearchAdapter(val data: List<RecentSearch>) :
+    RecyclerView.Adapter<RecentSearchAdapter.ViewHolder>() {
 
-    private lateinit var _binding : RecentSearchItemBinding
 
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view){}
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        _binding = RecentSearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        //val view = LayoutInflater.from(parent.context).inflate(R.layout.recent_search_item,parent,false)
-        return ViewHolder(_binding.root)
+
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.recent_search_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        _binding.citynametextview.text = data[position].cityName
+
 
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
-
-
 
 
 }
