1d794ba6850050114b1fae2dd13a6fcfae5328e6
Marker97 <chereche10@gmail.com>
2023-02-03 16:55:49 +0100

    fix_NextViewHolder

--- app/src/main/java/co/develhope/meteoapp/adapter/NextDaysViewHolder.kt
+++ app/src/main/java/co/develhope/meteoapp/adapter/NextDaysViewHolder.kt
@@ -5,12 +5,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.R
 import co.develhope.meteoapp.data.HomeScreenItems
+import co.develhope.meteoapp.databinding.Next5daysHomeScreenItemBinding
+import co.develhope.meteoapp.databinding.TitleHomeScreenItemBinding
 
-class NextDaysViewHolder(private val view : View) : RecyclerView.ViewHolder(view) {
-    private val textViewNextDays : TextView = view.findViewById(R.id.next5Days)
-
+class NextDaysViewHolder(private val binding: Next5daysHomeScreenItemBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(next5Days : HomeScreenItems.NextDays){
         //view.context.getString()
-        textViewNextDays.text = view.context.getString(R.string.next_days)
+        binding.next5Days.text = itemView.context.getString(R.string.next_days)
     }
 }
