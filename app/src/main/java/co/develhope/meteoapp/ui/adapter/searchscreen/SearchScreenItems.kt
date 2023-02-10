0e69d215ddb929fef90a523927913e9a3920bc9a
Mauro Lanini <mallettomachico@gmail.com>
2023-02-10 17:43:11 +0100

    change the parameter require for RecentSearch

--- app/src/main/java/co/develhope/meteoapp/ui/adapter/searchscreen/SearchScreenItems.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/adapter/searchscreen/SearchScreenItems.kt
@@ -1,7 +1,9 @@
 package co.develhope.meteoapp.ui.adapter.searchscreen
 
+import co.develhope.meteoapp.data.domainmodel.Place
+
 sealed class SearchScreenItems {
 
     data class RecentSearchTitle(val title : String) : SearchScreenItems()
-    data class RecentSearch(val cityName: String) : SearchScreenItems()
+    data class RecentSearch(val city: Place) : SearchScreenItems()
 }
