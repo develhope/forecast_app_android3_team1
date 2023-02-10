22577507c9ffde46575695a8b27e218da9d78315
Mauro Lanini <mallettomachico@gmail.com>
2023-02-10 18:46:12 +0100

    Implements search

--- app/src/main/java/co/develhope/meteoapp/ui/SearchFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/SearchFragment.kt
@@ -1,6 +1,9 @@
 package co.develhope.meteoapp.ui
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +23,10 @@ class SearchFragment : Fragment() {
     private var _binding : FragmentSearchBinding? = null
     val binding get() = _binding!!
 
+    private lateinit var adapter : SearchAdapter
+
+    private lateinit var recentSearch : List<Place>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,13 +38,13 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-
-
-        val adapter = SearchAdapter(transformDataForSearchAdapter(Datasource.loadRecentSearch()))
+        recentSearch = Datasource.loadRecentSearch()
+        adapter = SearchAdapter(transformDataForSearchAdapter(recentSearch))
         binding.recentsearchlist.adapter = adapter
         binding.recentsearchlist.layoutManager = LinearLayoutManager(view.context)
 
-
+        //register listener for search location
+        searchLocation()
     }
 
     override fun onDestroyView() {
@@ -55,4 +62,25 @@ class SearchFragment : Fragment() {
     }
 
 
+    private fun searchLocation() {
+        binding.searchBarLocation.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val filteredRecentSearch = recentSearch.filter {
+                    it.city.contains(s.toString())
+                }
+                adapter = SearchAdapter(transformDataForSearchAdapter(filteredRecentSearch))
+                binding.recentsearchlist.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+
+        })
+    }
 }
