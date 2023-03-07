584b6d9612558d68a9e21d022306cc48991e5b72
Mauro Lanini <mallettomachico@gmail.com>
2023-03-07 08:32:39 +0100

    add function for change the weather icon

--- app/src/main/java/co/develhope/meteoapp/ui/adapter/homescreen/CardViewHolder.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/adapter/homescreen/CardViewHolder.kt
@@ -2,6 +2,7 @@ package co.develhope.meteoapp.ui.adapter.homescreen
 
 import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.R
+import co.develhope.meteoapp.data.domainmodel.WeatherCondition
 import co.develhope.meteoapp.databinding.CardHomeScreenBinding
 
 class CardViewHolder(private val binding: CardHomeScreenBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -12,13 +13,15 @@ class CardViewHolder(private val binding: CardHomeScreenBinding) : RecyclerView.
         binding.maxTempCard.text = itemView.context.getString(R.string.tempMax, cardDetail.forecastSummary.forecast.maxTemp.toString())
         binding.rainfallPercent.text = itemView.context.getString(R.string.rainfallPercent, cardDetail.forecastSummary.forecast.rainfall.toString())
         binding.windKmhCard.text = itemView.context.getString(R.string.windKmh, cardDetail.forecastSummary.forecast.wind.toString())
+        binding.weathericon.setImageResource(decodeWeatherIcon(cardDetail.forecastSummary.forecast.weatherCondition))
+    }
 
-
-
-
-
-
-
+    private fun decodeWeatherIcon(weatherCondition: WeatherCondition) : Int {
+        return when(weatherCondition){
+            WeatherCondition.SUNNY -> R.drawable.sun_icon
+            WeatherCondition.RAIN -> R.drawable.rain_icon
+            else -> R.drawable.cloudy_icon
+        }
     }
 
 
